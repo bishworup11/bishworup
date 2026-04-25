24fc2118e12e41595204c2fd11b9bde5f8877ed2
bishworup11 <bishworupmollik@gmail.com>
2026-04-25 14:41:45 +0600

    static site generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
+  ssr: true,
+  nitro: {
+    preset: 'static',
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'],
+    },
+  },
 })
